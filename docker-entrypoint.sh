ff6de8d51937ce3289a4303cefed71cc8d81d045
Christos Sidiropoulos <csidirop@runbox.com>
2024-07-15 17:15:20 +0200

    Add plugins
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,15 @@ wait-for-it -t 0 ${DB_ADDR}:${DB_PORT}
 cd /var/www/html
 php setup/setup.php install /var/www/config.json --yes
 
+# Add plugin directory:
+mkdir -p Customizing/global/plugins/Services/Repository/RepositoryObject
+cd Customizing/global/plugins/Services/Repository/RepositoryObject
+
+# Add optional plugins:
+git clone -b release_7 --single-branch https://github.com/srsolutionsag/H5P.git H5P
+git clone -b release_7 --single-branch https://github.com/Minervis-GmbH/BigBlueButton-Ilias-Plugin.git BigBlueButton/
+git clone -b release7 --single-branch https://github.com/internetlehrer/MultiVc MultiVc
+
 # Finished:
 echo 'Ready for setup: http://localhost/'
 echo 'root:homer'

a8cf566ea5f0393b231ac263dffe6496fde45fa0
Christos Sidiropoulos <dev@csidirop.de>
2024-11-14 16:37:46 +0100

    Initial update to Ilias `release_10`
    Signed-off-by: Christos Sidiropoulos <dev@csidirop.de>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,14 +6,15 @@ echo 'Running startup script:'
 wait-for-it -t 0 ${DB_ADDR}:${DB_PORT}
 
 # Install ILIAS:
-php setup/setup.php install /var/www/config.json --yes
+php components/ILIAS/Setup/Setup.php install /var/www/config.json --yes
+# php components/ILIAS/setup_/setup_.php install /var/www/config.json --yes
 
 # Add plugin directory:
 mkdir -p Customizing/global/plugins/Services/Repository/RepositoryObject
 cd Customizing/global/plugins/Services/Repository/RepositoryObject
 
 # Add optional plugins:
-git clone -b release_9 --single-branch https://github.com/srsolutionsag/H5P.git H5P
+# git clone -b release_9 --single-branch https://github.com/srsolutionsag/H5P.git H5P
 # git clone -b release_9 --single-branch https://github.com/Minervis-GmbH/BigBlueButton-Ilias-Plugin.git BigBlueButton/
 # git clone -b release9 --single-branch https://github.com/internetlehrer/MultiVc MultiVc
 # ... Add more plugins here ...
